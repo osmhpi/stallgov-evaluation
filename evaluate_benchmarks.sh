7dabb2178d4a8380248d14d18ea5cc98efdc4df2
Leon Matthes <leon@matthes.biz>
2022-01-13 17:24:12 +0100

    evaluate_benchmarks: Quality of live improvements
    Add an plot-evaluation script based on plot.py to plot frequency-graphs

--- evaluate_benchmarks.sh
+++ evaluate_benchmarks.sh
@@ -3,8 +3,13 @@
 set -e
 
 if [[ ! -f "frequencies.config" ]]; then
-  echo "frequencies.config does not exist!"
-  echo "It must be a file with all frequencies to evaluate on the first line (separated by space)"
+  echo "Error: frequencies.config does not exist!"
+  echo "frequencies.config must be a file with all frequencies"
+  echo "to evaluate on the first line (separated by space)"
+  echo "See frequencies.config.example for an example file."
+  echo ""
+  echo "You can get access to all available frequencies with:"
+  echo "        cpupower frequency-info"
   exit 1
 fi
 
@@ -26,7 +31,7 @@ do
   cpupower frequency-set -g memutil
   mkdir -p "$workload_name-memutil-log"
   # Copying the current log will clear it, so we start with a fresh log
-  cp /sys/kernel/debug/memutil/log > /dev/null
+  cp /sys/kernel/debug/memutil/log /dev/null
   # Start the copy-log, as well as pinpoint-stats commands in the background
   # and terminate both, if one of them exits.
   # See: https://unix.stackexchange.com/questions/231676/given-two-background-commands-terminate-the-remaining-one-when-either-exits

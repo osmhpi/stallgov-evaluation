60a054a13ba715c0d382e4571aa7a13119b31a39
Leon Matthes <leon@matthes.biz>
2022-01-20 16:43:25 +0100

    evaluation: add plot-log to plot-multiple

--- plot-multiple.sh
+++ plot-multiple.sh
@@ -11,14 +11,17 @@ fi
 
 folder="$1"
 
-mkdir -p /tmp/plot-multiple
+mkdir -p /tmp/plot-multiple/plot-evaluation
+mkdir -p /tmp/plot-multiple/plot-log
 
 # Iterate over all arguments but the first
 for workload in "${@:2:$#}"
 do
-  ./plot-evaluation.py -sd --second_plot time --title "$workload" "$workload" "$folder" "/tmp/plot-multiple/$workload.png"
+  ./plot-evaluation.py -sd --second_plot time --title "$workload" "$workload" "$folder" "/tmp/plot-multiple/plot-evaluation/$workload.png"
+  ../utils/plot-log.py --title "$workload" "$folder/$workload-memutil-log" "/tmp/plot-multiple/plot-log/$workload.png"
 done
 
-gm montage -mode concatenate -tile 3x /tmp/plot-multiple/*.png out.png
+gm montage -mode concatenate -tile 3x /tmp/plot-multiple/plot-evaluation/*.png evaluation.png
+gm montage -mode concatenate -tile 3x /tmp/plot-multiple/plot-log/*.png log.png
 
 rm -rf /tmp/plot-multiple

f3d5d106dd0d6ab8ef7d8853727697ec42b9ca33
Erik Griese <erik.griese@student.hpi.de>
2022-03-22 13:48:26 +0100

    Uncomments line for test dependencies as they seem to be needed

--- openbenchmarking/setup.sh
+++ openbenchmarking/setup.sh
@@ -38,7 +38,7 @@ WORKLOAD_DIRECTORY="../utils/workloads"
 mkdir -p "$WORKLOAD_DIRECTORY"
 for TEST in $TESTS; do
     echo "installing $TEST"
-    # phoronix-test-suite install-dependencies ${TEST}
+    phoronix-test-suite install-dependencies ${TEST}
     phoronix-test-suite batch-install $TEST
     workload_file="$WORKLOAD_DIRECTORY/$(echo $TEST | sed -e 's/\//_/g')"
     echo -e "#!/usr/bin/env bash\nphoronix-test-suite batch-run $TEST" > "$workload_file"

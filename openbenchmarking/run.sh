67b463e74b48d52bd4409e481b6969943b282397
Erik Griese <erik.griese@student.hpi.de>
2022-03-23 13:54:31 +0100

    Adjusts openbenchmarking runscript to set options and use static runcount

--- openbenchmarking/run.sh
+++ openbenchmarking/run.sh
@@ -12,7 +12,10 @@ TEST_LIST_FILE="openbenchmarking.tests"
 # this file saves the progress in the test set
 PROGRESS_FILE="running_tests.lock"
 
-export TEST_RUNS=1
+export TEST_RUNS=3
+export PRESET_OPTIONS="viennacl.test=1;webp.encode=3;svt-vp9.tune=1;svt-vp9.input=0;mcperf.method=1;mcperf.connections=0;multichase.run-test=0;mbw.run-test=0;mbw.array-size=3;openssl.algo=0;redis.test=0;embree.bin=0;embree.model=0;sysbench.run-test=2"
+
+declare -A TIMES_TO_RUN_OVERRIDE=( ["multichase"]=3)
 
 if [ ! -s "$PROGRESS_FILE" ]; then
     echo "Starting new run"
@@ -28,6 +31,14 @@ for ((i=1;i<=TEST_COUNT;i++)); do
     # take current test from first line
     read -r TEST<"$PROGRESS_FILE"
     echo "Running test $i/$TEST_COUNT: $TEST"
+
+    # Override how often the test should be run
+    if [ ${TIMES_TO_RUN_OVERRIDE[$TEST]} ]
+    then
+        export FORCE_TIMES_TO_RUN=${TIMES_TO_RUN_OVERRIDE[$TEST]}
+    else
+        export FORCE_TIMES_TO_RUN=1 # Default run just 1 time
+    fi
 
     (cd .. && ./evaluate_benchmarks.sh "utils/workloads/$TEST")
 

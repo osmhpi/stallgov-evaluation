53bdd0d00512fa72d517e749ce2cbd4c352e6a2f
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2022-03-24 21:24:22 +0100

    [evaluation] add simpler plot.sh

---
+++ plot.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+BENCH_RUN_FOLDER=""
+WORKLOAD_NAMES=""
+OUTPUT_FILE="output.png"
+NUMBER_FORMAT_PARAM="sd"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+		--number_format)
+            case "$2" in
+                EN)
+                    NUMBER_FORMAT_PARAM="sd"
+                    ;;
+                DE)
+                    NUMBER_FORMAT_PARAM="gf"
+                    ;;
+                *)
+                    echo "invalid number format - possible values: EN, DE"
+                    exit 1
+                    ;;
+            esac
+			shift # past argument
+			shift # past value
+			;;
+		--help)
+			echo "./plot.sh <bench_run_folder> [workload_names...] --number_format=[EN]/DE"
+			exit 0
+			;;
+		*) # unknown option
+			POSITIONAL+=("$1") # save it in an array for later
+			shift # past argument
+			;;
+	esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [ "$#" -le "0" ]; then
+    echo "./plot.sh <bench_run_folder> [workload_names...] --number_format=[EN]/DE"
+    exit 1
+fi
+
+BENCH_RUN_FOLDER="$1"
+if [ "$#" -gt 1 ]; then
+    WORKLOAD_NAMES="${@:2:$#}"
+else
+    for POSSIBLE_WORKLOAD in `ls $BENCH_RUN_FOLDER`; do
+        if [ -f "$BENCH_RUN_FOLDER/$POSSIBLE_WORKLOAD/$POSSIBLE_WORKLOAD-memutil.txt" ] \
+        && [ -f "$BENCH_RUN_FOLDER/$POSSIBLE_WORKLOAD/$POSSIBLE_WORKLOAD-schedutil.txt" ]; then
+            WORKLOAD_NAMES="$WORKLOAD_NAMES $POSSIBLE_WORKLOAD"
+        fi
+    done
+fi
+
+for WORKLOAD_NAME in $WORKLOAD_NAMES; do
+    TEST_RUN_FOLDER="$BENCH_RUN_FOLDER/$WORKLOAD_NAME"
+    echo "Plotting $TEST_RUN_FOLDER"
+    python plot-evaluation.py "$WORKLOAD_NAME" "$TEST_RUN_FOLDER" "$TEST_RUN_FOLDER/$OUTPUT_FILE" -sp time -$NUMBER_FORMAT_PARAM
+done
